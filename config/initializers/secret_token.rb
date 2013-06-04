246816e5d5f35a7d04b2478bbf1fedb82ee79d15
Reginald Pawle <rbpawle@gmail.com>
2013-06-04 13:12:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcardizer::Application.config.secret_token = '574972a417746a028ea7001463790eceaf0b206124573093a4c426bfda84e7bb44117c2928b676c523d276d1c1cedea381273016d12447635c2b542fe2bb1201'
