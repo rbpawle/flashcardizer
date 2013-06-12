a2968fd4a585ce06eb4c669ee92bd4c805f2fc14
Reginald Pawle <rbpawle@gmail.com>
2013-06-12 16:23:45 -0400

    removed migration file

--- db/migrate/20130611151943_remove_topic_from_flashcard.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTopicFromFlashcard < ActiveRecord::Migration
-  def up
-    remove_column :flashcards, :topic
-  end
-
-  def down
-    add_column :flashcards, :topic, :string
-  end
-end
